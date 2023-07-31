28716f7ffb65cc1538fe3f721de1d06790793e60
NZB3 <ni.bukhalov@gmail.com>
2023-07-31 19:03:51 +0300

    delete formating print

--- measure.go
+++ measure.go
@@ -1,14 +1,11 @@
 package measure
 
 import (
-	"fmt"
 	"time"
 )
 
 func Measure(fn func()) time.Duration {
 	start := time.Now()
 	fn()
-	elapsed := time.Since(start)
-	fmt.Printf("Time taken: %s\n\n", elapsed)
-	return elapsed
+	return time.Since(start)
 }
